8d655dfb4271f62ad64724fb4a481e9100cf73a9
Vladimir <penkinv@gmail.com>
2011-10-04 12:40:22 +0400

    oooops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Plist::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.deconfig.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
