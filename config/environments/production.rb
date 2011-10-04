0cb86d4eeba3026e5f3dbcd5a3441e53471ce651
Vladimir <penkinv@gmail.com>
2011-10-04 12:39:32 +0400

    oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Plist::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com'
+    :domain               => 'gmail.com',
     :user_name            => 'plisttv@gmail.com',
     :password             => 'bakadesu',
     :authentication       => 'plain',
