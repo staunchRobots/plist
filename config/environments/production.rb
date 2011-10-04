d6c4f03435cacba0309a55bae92645a1fa741c3a
Vladimir <penkinv@gmail.com>
2011-10-04 12:54:56 +0400

    -domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,6 @@ Plist::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com',
     :user_name            => 'plisttv@gmail.com',
     :password             => 'bakadesu',
     :authentication       => 'plain',
