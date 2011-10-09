d5606c7aae631d2f6459bd4a8dfbac164ef92807
Vladimir <penkinv@gmail.com>
2011-10-09 15:13:49 +0400

    Cleaning up playlist title

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,14 +3,14 @@ class Playlist < ActiveRecord::Base
   # has_many :videos, :through => :playlist_videos
   has_many :videos, :order => 'sort ASC'
   belongs_to :user
-  
+
   scope :featured, where(:featured => true)
   scope :common, where(:featured => false)
   scope :published, where(:published => true)
   scope :drafts, where(:published => false)
   scope :hot, where(:hot => true)
   scope :recent, lambda {|n| order_by("created_at DESC").limit(n) }
-       
+
   def add_video(yt_url, is_url = true)
     ytid = is_url ? extract_ytid(yt_url) : yt_url
     unless videos.collect(&:ytid).include? ytid
@@ -28,12 +28,11 @@ class Playlist < ActiveRecord::Base
       update_attribute(:thumb, Youtube.get_thumbnail_url(tmp.ytid))
     end
   end
-  
+
   def extract_ytid(yt_url)
     tmpid = yt_url.gsub(/.*\?/, '').split('&').find{|r| r.match /v=.*/}
-    tmpid[2..-1]    
+    tmpid[2..-1]
   end
-  
 
   def update_videos_sort_order(params)
     order = ids_to_sorting_hash(params)
@@ -46,6 +45,10 @@ class Playlist < ActiveRecord::Base
   def ids_to_sorting_hash(ids)
     index = 0
     ids.collect{|value| index += 1; {value.to_i => index};}.reduce(&:merge)
+  end
+
+  def description
+    self.attributes['description'] || "Enter a brief yet touching description by clicking this beautiful options button"
   end
 
 end
